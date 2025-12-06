a2cd1c5ad0b6fd1b87cc3f08798ac1b487dbb16f
Your Name <you@example.com>
2025-12-06 17:45:52 +0500

    okk

--- main.go
+++ main.go
@@ -127,6 +127,7 @@ func main() {
 			}
 		}
 	}
+	words  = FixArticles(words)
 		lines[li] = strings.Join(words, " ")
 	}
 
@@ -241,4 +242,63 @@ func ReattachPunc(words []string) []string {
 		}
 	}
 	return res
+}
+
+// FixArticles adjusts "a"/"an" based on the following word
+func FixArticles(words []string) []string {
+	vowels := "aeiouAEIOU"
+	// list of special words where 'h' is silent
+	silentH := map[string]bool{
+		"hour":   true,
+		"honest": true,
+		"honor":  true,
+		"heir":   true,
+	}
+
+	for i := 0; i < len(words)-1; i++ {
+		val := words[i]
+		next := words[i+1]
+
+		if !isWord(next) {
+			continue
+		}
+
+		lowerNext := strings.ToLower(next)
+		if strings.EqualFold(val, "a") || strings.EqualFold(val, "an") {
+			needsAn := false
+			firstChar := next[0]
+
+			if strings.Contains(vowels, string(firstChar)) {
+				needsAn = true
+			} else if silentH[lowerNext] {
+				needsAn = true
+			} else {
+				needsAn = false
+			}
+
+			// apply the change preserving capitalization
+			if needsAn {
+				if strings.EqualFold(val, "a") {
+					if val == "a" {
+						words[i] = "an"
+					} else if val == "A" {
+						words[i] = "An"
+					} else if val == "AN" {
+						words[i] = "AN"
+					}
+				}
+			} else {
+				if strings.EqualFold(val, "an") {
+					if val == "an" {
+						words[i] = "a"
+					} else if val == "An" {
+						words[i] = "A"
+					} else if val == "AN" {
+						words[i] = "A"
+					}
+				}
+			}
+		}
+	}
+	return words
 }
